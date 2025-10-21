136813d141256c67b69e24ddeab3df0546c35614
Alexis Vanautgaerden <alvanaut@shi-r10-p3.s19.be>
2025-10-21 12:45:44 +0200

    Add basic struct

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -8,5 +8,21 @@
 # include <sys/time.h>
 # include "../libft/libft.h"
 
+typedef struct s_player {
+	double player_x;
+	double player_y;
+	double dir_x;
+	double dir_y;
+	double plane_x;
+	double plane_y;
+
+}			t_player;
+
+typedef struct s_data {
+	void *win;
+	void *mlx;
+	void *addr;
+	t_player player;
+}			t_data;
 
 #endif
